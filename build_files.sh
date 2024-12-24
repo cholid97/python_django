7bdea4c5b6c7025f953395cd9c38f083c98897d5
cholid97 <cholidnurfachri@gmail.com>
2024-12-24 12:21:33 +0700

    first commit

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+echo " BUILD START"
+python3.9 -m install -r requirements.txt
+python3.9 manage.py collectstatic
+echo "BUILD END"
